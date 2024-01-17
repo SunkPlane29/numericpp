8a34fb2ef1589cd77ea4bb3a92fcac21c9872cf3
André <andrezito300@gmail.com>
2024-01-17 19:56:08 -0300

    change numericpp.h

--- numericpp.h
+++ numericpp.h
@@ -1,8 +1,30 @@
 #ifndef NUMERICPP_H
 #define NUMERICPP_H
 
-#include "util.h"
-#include "root.h"
-#include "gquad.h"
+//NOTE: this is also a hack to allow the simple compilation of the files and
+// then the use by only copying this header and the archive file
+
+#include <vector>
+#include <string>
+#include <functional>
+
+unsigned long bin(int, int);
+double bin(double, double);
+void writedata(std::string, std::vector<std::vector<double>>, int precision=8);
+
+std::vector<double> linspace(double, double, int);
+std::vector<double> operator%(std::function<double(double)>, std::vector<double>);
+std::vector<double> operator*(std::vector<double>, std::vector<double>);
+std::vector<double> operator*(std::vector<double>, double);
+std::vector<double> operator*(double, std::vector<double>);
+std::vector<double> operator+(std::vector<double>, std::vector<double>);
+std::vector<double> operator+(std::vector<double>, double);
+std::vector<double> operator+(double, std::vector<double>);
+std::vector<double> operator-(std::vector<double>, std::vector<double>);
+std::vector<double> operator-(std::vector<double>, double);
+std::vector<double> operator-(double, std::vector<double>);
+std::vector<double> operator/(std::vector<double>, std::vector<double>);
+std::vector<double> operator/(std::vector<double>, double);
+std::vector<double> operator^(std::vector<double>, double);
 
 #endif
